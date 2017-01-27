0e1af492f56667bcc7a183f434f657f7acd116d3
max-matveev <max.matveevv@gmail.com>
2017-01-27 16:11:36 +0600

    Start project

---
+++ Doozyrama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doozyrama
+//
+//  Created by Max on 27.01.17.
+//  Copyright © 2017 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
